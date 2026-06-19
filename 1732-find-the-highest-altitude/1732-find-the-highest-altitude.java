4ef5ac89bf6edcf9874b83664ff8e91165013829
Kirtika B <kirtikakirtika545@gmail.com>
2026-06-19 18:31:48 +0530

    Time: 1 ms (4.86%), Space: 42.8 MB (93.81%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int n=gain.length;
+        int mx=0;
+        for(int i=0;i<=n;i++){
+            int alt=0;
+            for(int j=0;j<i;j++){
+                alt +=gain[j];
+            }
+            mx=Math.max(mx, alt);
+        }
+        return mx;
+    }
+}
